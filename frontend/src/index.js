2bc200c2a36795f0387a0cff41aec34486011128
Yuval Nakav <yuvalnakav1@gmail.com>
2021-02-26 17:19:44 +0200

    added startGame function, added aditional css deleted card photos
    startGame deals the cards and pushing them to the new decks,
    css:  made card divs, adjusted sideways cards

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-const SUITS = [`♥`, `♦`, `♣`, `♠`];
-const RANKS = [`Ace`, "2", "3", "4", "5", "6", "7", "8", "9", "10", `Jack`, `Queen`, `King`];
+const SUITS = [`♠`, `♥`, `♦`, `♣`];
+const RANKS = [`A`, "2", "3", "4", "5", "6", "7", "8", "9", "10", `J`, `Q`, `K`];
 class Card {
     constructor(rank, suit, isJocker = false) {
         this.rank = rank;
@@ -9,15 +9,28 @@ class Card {
     get color(){
         return this.suit === `♣` || this.suit === `♠`? `black` : `red`;
     }
-    createCard(card){
-        const cardDiv = document.createElement("div");
-        cardDiv.innerText =this.suit;
-        cardDiv.classList.add("card");
+    createCard(){
+            const cardDiv = document.createElement("div");
+            cardDiv.innerText = this.suit
+            cardDiv.classList.add("card", this.color);
+            cardDiv.dataset.value = `${this.rank} ${this.suit}`;
+            return cardDiv;
+        
     }
-}
+};
 class Jocker extends Card {
-    constructor(rank = "Jocker", color, isJocker = true){
+    super(rank = "Jocker", suit, isJocker){
         this.isJocker = isJocker;
+        this.rank = rank;
+    }
+    createCard(){
+            const cardDiv = document.createElement("div"); 
+            cardDiv.innerText = `${this.rank}`;
+            cardDiv.classList.add("card", this.suit, this.rank);
+            cardDiv.dataset.value = `${this.rank}`;
+            return cardDiv;
+
+        
     }
 }
 class Deck{
@@ -50,14 +63,54 @@ class Deck{
         }
         return hand;
     }
+};
+class PlayerDeck extends Deck{
+    super(){
+        this.cards = [];
+    }
+    createDeck(){
+        this.cards.push(Card.cards.pop());
+    }
+}
+class TableDeck extends Deck{
+    super(){
+        this.cards = [];
+    }
+    createDeck(){
+        this.cards.push(Card.cards.pop())
+    }
+}
+class PileDeck extends Deck{
+    super(){
+            this.cards = [];
+    }
+        createDeck(){
+            this.cards.push(Card.cards.pop())
+        }
 }
 
-
-const deck = new Deck();
-deck.createDeck()
-const blackJocker = new Jocker("0", "black", );
-const redJocker = new Jocker("red");
-deck.cards.push(blackJocker, redJocker);
-deck.shuffle()
-console.log(deck)
- 
+function startGame(){
+    const deck = new Deck();
+    deck.createDeck()
+    const blackJocker = new Jocker("☻", "black", true);
+    const redJocker = new Jocker("☻", "red", true);
+    deck.cards.push(blackJocker, redJocker);
+    deck.shuffle();
+    for (let i = 1; i <= 4; i++){
+        let player = document.getElementById("player"+i);
+        let playerDeck = new PlayerDeck();
+        for(let j = 0; j < 5; j++){
+            let newCard = deck.cards[0].createCard();
+            player.append(newCard);
+            playerDeck.cards.push(deck.cards.shift());
+            if(i === 2 || i === 3){
+                newCard.classList.add("sideways");
+            }
+        }
+        let player3 = document.getElementById("player3")
+        let player2 = document.getElementById("player2")
+        console.log(playerDeck.cards);
+        console.log(deck.cards);
+    }
+}
+startGame()

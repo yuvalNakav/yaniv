8757e896970d65c7bdbed11b13edd29a82d85218
Yuval Nakav <yuvalnakav1@gmail.com>
2021-02-27 11:47:46 +0200

    added card values and backs, added simple event listener for future develup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,28 @@
 const SUITS = [`♠`, `♥`, `♦`, `♣`];
-const RANKS = [`A`, "2", "3", "4", "5", "6", "7", "8", "9", "10", `J`, `Q`, `K`];
+const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const VALUES = {
+    "☻" : 0,
+    "A": 1,
+    "2" :2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10" : 10,
+    "J" : 10,
+    "Q" : 10,
+    "K" : 10
+}
+
 class Card {
-    constructor(rank, suit, isJocker = false) {
+    constructor(rank, suit, isJocker = false, value) {
         this.rank = rank;
         this.suit = suit;
         this.isJocker = isJocker;
+        this.value = value;
     }
     get color(){
         return this.suit === `♣` || this.suit === `♠`? `black` : `red`;
@@ -14,8 +32,12 @@ class Card {
             cardDiv.innerText = this.suit
             cardDiv.classList.add("card", this.color);
             cardDiv.dataset.value = `${this.rank} ${this.suit}`;
+            cardDiv.value = VALUES[this.rank];
             return cardDiv;
         
+    }
+    pop(){
+
     }
 };
 class Jocker extends Card {
@@ -26,8 +48,9 @@ class Jocker extends Card {
     createCard(){
             const cardDiv = document.createElement("div"); 
             cardDiv.innerText = `${this.rank}`;
-            cardDiv.classList.add("card", this.suit, this.rank);
+            cardDiv.classList.add("card", this.suit);
             cardDiv.dataset.value = `${this.rank}`;
+            cardDiv.value = VALUES[this.rank];
             return cardDiv;
 
         
@@ -37,7 +60,7 @@ class Deck{
     constructor(){
         this.cards = [];
     }
-    createDeck(value, sign, isJocker = false){
+    createDeck(){
             RANKS.forEach(rank => {
                 SUITS.forEach(suit =>{
                     this.cards.push( new Card(rank, suit, false));
@@ -87,12 +110,12 @@ class PileDeck extends Deck{
         createDeck(){
             this.cards.push(Card.cards.pop())
         }
-}
+    }
+const blackJocker = new Jocker("☻", "black", true);
+const redJocker = new Jocker("☻", "red", true);
 let deck = new Deck();
 function startGame(){
     deck.createDeck()
-    const blackJocker = new Jocker("☻", "black", true);
-    const redJocker = new Jocker("☻", "red", true);
     deck.cards.push(blackJocker, redJocker);
     deck.shuffle();
     for (let i = 1; i <= 4; i++){
@@ -106,8 +129,6 @@ function startGame(){
                 newCard.classList.add("sideways");
             }
         }
-        // let player3 = document.getElementById("player3")
-        // let player2 = document.getElementById("player2")
     }
     flipTableDeck();
 }
@@ -119,4 +140,25 @@ function flipTableDeck(){
     slot.append(newCard);
     deck.cards.shift();
     tableDeck.cards = deck.cards;
+    let nextCard = document.createElement("div");
+    nextCard.classList.add("upside-down");
+    slot.append(nextCard);
+};
+const playerDiv = Array.from(document.getElementsByClassName("player"));
+let cardArr = Array.from(document.getElementsByClassName("card"));
+let cardDiv = document.getElementsByClassName("card");
+const tableDiv = document.getElementById("slot");
+const player1 =  document.getElementById("player1");
+const player2 =  document.getElementById("player2");
+const player3 =  document.getElementById("player3");
+const player4 =  document.getElementById("player4");
+player1.addEventListener("click", throwCard);
+function throwCard(e){
+    let card = e.target;
+    if(card.classList.contains("card")){
+        // if (card.classList[0] === "card") {
+        //     playerDeck.cards.slice()
+        // }
+        console.log(card)
+    }
 }
